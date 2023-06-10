0d072ae96a26a398b15005bcffbdbde8650c3284
MasterKing0806 <canhhuy.phung@googlemail.com>
2023-06-10 19:56:24 +0200

    lolfix

--- Bashscripts/lolfix.sh
+++ Bashscripts/lolfix.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-hyprctl keyword monitor DP-1,2560x1440@165,0x1540,1
+
+
+# set the expected x position of the monitor by your preference
+expect_y=1540
+# index 0 is the first monitor in your hyprland.conf
+curr_y=$(hyprctl -j monitors | jq '.[0].y') 
+if [ $curr_y -ne $expect_y ]
+then
+    hyprctl keyword monitor DP-1,2560x1440@165,0x${expect_y},1
+else
+    expect_y=$(($expect_y - 100))
+    hyprctl keyword monitor DP-1,2560x1440@165,0x${expect_y},1
+fi
--- Bashscripts/lolfix2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-hyprctl dispatch killactive
-
-hyprctl keyword monitor DP-1,2560x1440@165,0x1440,1
