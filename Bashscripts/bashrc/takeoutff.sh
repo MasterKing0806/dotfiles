88d1c4f26a7861951dd0749f349a435eff1acf2c
MasterKing0806 <canhhuy.phung@googlemail.com>
2023-11-26 18:15:45 +0100

    26.11.23

--- Bashscripts/bashrc/takeoutff.sh
+++ Bashscripts/bashrc/takeoutff.sh
@@ -13,3 +13,14 @@ fi
 
 }
 take
+ggdr(){
+
+if [[ "$AUTO" == *"firefox"* ]]; then
+        firefox https://drive.google.com/drive/folders/1H3yzQRfY5beDW4rXad_qpUpDYbTPx3I0
+else
+        firefox --new-window https://drive.google.com/drive/folders/1H3yzQRfY5beDW4rXad_qpUpDYbTPx3I0
+fi
+
+
+}
+ggdr
