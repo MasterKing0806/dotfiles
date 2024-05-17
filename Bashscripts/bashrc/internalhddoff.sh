44bb9a5c6316962d0d3a331373ec7696f6ed2c30
MasterKing0806 <canhhuy.phung@googlemail.com>
2024-05-17 14:19:03 +0200

    17.05.24

---
+++ Bashscripts/bashrc/internalhddoff.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+sudo udisksctl unmount -b /dev/sdc3
+sudo udisksctl power-off -b /dev/sdc
--- Bashscripts/hyprlandlogin.sh
+++ Bashscripts/hyprlandlogin.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-	source /home/ca/Bashscripts/bashrc/internalhddoff # to turn off internal hdd with windows on; remove when everything is on ssd
 	rm /home/ca/.config/waybar/config
 	rm /home/ca/.config/waybar/style.css
 	cp /home/ca/.config/waybar/waybar_hypr/config /home/ca/.config/waybar
 	cp /home/ca/.config/waybar/waybar_hypr/style.css /home/ca/.config/waybar
 	exec Hyprland
-
