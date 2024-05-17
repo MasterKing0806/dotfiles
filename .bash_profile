ebadacf0acf70495614faea3577f40fc89d5a8cf
MasterKing0806 <canhhuy.phung@googlemail.com>
2024-05-17 14:11:10 +0200

    17.05.24

--- .bash_profile
+++ .bash_profile
@@ -3,8 +3,8 @@
 #
 
 if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-#    source Bashscripts/wayland.sh
-#    source Bashscripts/swaylogin.sh
+   # source Bashscripts/wayland.sh
+   # source Bashscripts/swaylogin.sh
     source Bashscripts/hyprlandlogin.sh
 fi
 
--- Bashscripts/hyprlandlogin.sh
+++ Bashscripts/hyprlandlogin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+	source /home/ca/Bashscripts/bashrc/internalhddoff # to turn off internal hdd with windows on; remove when everything is on ssd
 	rm /home/ca/.config/waybar/config
 	rm /home/ca/.config/waybar/style.css
 	cp /home/ca/.config/waybar/waybar_hypr/config /home/ca/.config/waybar
