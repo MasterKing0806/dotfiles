5ec304544855bc062aba40289f1931956a1af951
MasterKing0806 <canhhuy.phung@googlemail.com>
2025-12-05 22:30:20 +0100

    05.12.25

--- Bashscripts/server/docker_update_solo.sh
+++ Bashscripts/server/docker_update_solo.sh
@@ -3,14 +3,15 @@
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting update"
 mapfile -t files < <(sudo find /home/caca/ -type f -name "compose.yaml")
 #echo "${files[0]}"   # first file
-for file in "${files[@]}"; do
-     echo "Stopping containers in $file"
-     docker compose -f "$file" down
-done
 
 for file in "${files[@]}"; do
      echo "Pulling containers in $file"
      docker compose -f "$file" pull
+done
+
+for file in "${files[@]}"; do
+     echo "Stopping containers in $file"
+     docker compose -f "$file" down
 done
 
 for file in "${files[@]}"; do
