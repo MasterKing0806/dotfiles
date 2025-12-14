f7568bf88eb234609f6940d2c71f4628ed9b5b3e
MasterKing0806 <canhhuy.phung@googlemail.com>
2025-12-14 21:31:28 +0100

    14.12.25

--- Bashscripts/bashrc/takeoutff.sh
+++ Bashscripts/bashrc/takeoutff.sh
@@ -4,11 +4,20 @@ AUTO=$(hyprctl clients | grep firefox)
 ggfs(){
 
 if [[ "$AUTO" == *"firefox"* ]]; then
-        firefox https://drive.google.com/drive/folders/1H3yzQRfY5beDW4rXad_qpUpDYbTPx3I0
+         
+        # Grdrive
+        #firefox https://drive.google.com/drive/folders/1H3yzQRfY5beDW4rXad_qpUpDYbTPx3I0
 
-        firefox https://takeout.google.com/
+        #firefox https://takeout.google.com/
+        read -p "Sync Joplin " JOP1
+        firefox http://caserver.fritz.box:8080/login
+    
 else
-        firefox --new-tab -url https://drive.google.com/drive/folders/1H3yzQRfY5beDW4rXad_qpUpDYbTPx3I0 --new-tab -url https://takeout.google.com/
+        # Gdrive
+        #firefox --new-tab -url https://drive.google.com/drive/folders/1H3yzQRfY5beDW4rXad_qpUpDYbTPx3I0 --new-tab -url https://takeout.google.com/
+
+        read -p "Sync Joplin " JOP2
+        firefox --new-tab -url http://caserver.fritz.box:8080/login
 fi
 
 
