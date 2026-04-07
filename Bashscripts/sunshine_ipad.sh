49efe6eb6b8327e8271999c748d7984b68c0c2c3
MasterKing0806 <canhhuy.phung@googlemail.com>
2026-04-07 13:47:26 +0200

    07.04.26

---
+++ Bashscripts/sunshine_ipad.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+	cp /home/ca/.config/hyprland.conf /home/ca/.config/hypr/sunshine/hyprland_og.conf
+	rm /home/ca/.config/hypr/hyprland.conf
+	cp /home/ca/.config/hypr/sunshine/hyprland_sunshine.conf /home/ca/.config/hypr/hyprland.conf
