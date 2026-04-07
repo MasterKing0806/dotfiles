37f3f8d0260d24bfbc038da8a126d96b37dda243
MasterKing0806 <canhhuy.phung@googlemail.com>
2026-04-07 14:34:56 +0200

    07.04.26

--- Bashscripts/sunshine_ipad.sh
+++ Bashscripts/sunshine_ipad.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-	cp /home/ca/.config/hyprland.conf /home/ca/.config/hypr/sunshine/hyprland_og.conf
-	rm /home/ca/.config/hypr/hyprland.conf
+if ! systemctl --user is-active --quiet sunshine; then
+	cp /home/ca/.config/hypr/hyprland.conf /home/ca/.config/hypr/sunshine/hyprland_og.conf
 	cp /home/ca/.config/hypr/sunshine/hyprland_sunshine.conf /home/ca/.config/hypr/hyprland.conf
+    hyprctl output create headless DP-3
+    systemctl --user start sunshine
+    lganaus 
+else
+	cp /home/ca/.config/hypr/sunshine/hyprland_og.conf /home/ca/.config/hypr/hyprland.conf
+    systemctl --user stop sunshine
+    hyprctl output destroy DP-3
+    hyprctl reload
+    lganaus
+fi
